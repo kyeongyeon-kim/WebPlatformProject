85481cd3afa043641348ad6417a83acd309f2781
도연 <doyeon@naver.com>
2023-02-16 15:53:35 +0900

    도연 commit

---
+++ ohgym-back/src/ohgym/user/infoChangeServlet.java
@@ -0,0 +1,26 @@
+package ohgym.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/change")
+public class infoChangeServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+		resp.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+		resp.setHeader("Access-Control-Allow-Headers", "*");
+		
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		
+		
+	}
+
+}
