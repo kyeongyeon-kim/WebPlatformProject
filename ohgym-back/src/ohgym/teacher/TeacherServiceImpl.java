5fa59265f3c9c224e8cda63d839a9efa7e70107f
이은진 <dldmswls0910@naver.com>
2023-02-16 11:43:00 +0900

    commit

---
+++ ohgym-back/src/ohgym/teacher/TeacherServiceImpl.java
@@ -0,0 +1,73 @@
+package ohgym.teacher;
+
+import java.util.List;
+
+public class TeacherServiceImpl implements TeacherService {
+
+	@Override
+	public TeacherInfo createTeacherInfo(TeacherInfo info) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TeacherInfo> readTeacherInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeacherInfo updateTeacherInfo(TeacherInfo info) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeacherInfo deleteTeacherInfo(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String createExercise(String id, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateExercise(String id, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteExercise(String id, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeacherExercise createTeacherExercise(TeacherExercise exercise) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TeacherExercise> readTeacherExercise() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeacherExercise updateTeacherExercise(TeacherExercise exercise) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TeacherExercise deleteTeacherExercise(String id, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
