0410de4f916f465574c2ce3519fd8d13bf649678
이은진 <dldmswls0910@naver.com>
2023-02-16 16:14:37 +0900

    은진 commit

--- front/js/search.js
+++ front/js/search.js
@@ -1,23 +1,32 @@
-let name = document.getElementById("name");
-let introduction = document.getElementById("introduction");
-fetch("http://192.168.0.104:8080/ohgym-back/find") 
-            .then((resp) => resp.text())
-            .then((text) => {
-                // makeObj(text);
-                // console.log(text);
-                // const infoStr = text;
-                // const infoSplit = infoStr.split(", ");
-             
-                introduction.innerText = text;
-            });
+window.addEventListener("load", onload);
 
-
-// let teacherInfo {
+function onload(e) {
+    console.log("페이지 로딩 후 호출됩니다.");
     
-// };
-// let makeObj = function(x) {
-//     const str = x;
-//     const strSplit = str.split("TeacherInfo ");
-//     console.log(Array.isArray(strSplit));
-//     return strSplit[1];
-// };
+    fetch("http://192.168.0.104:8080/ohgym/find") 
+    .then((resp) => resp.json())
+    .then((arr) => {
+        let container = document.getElementById("profile-container");
+        let name = document.getElementById("name");
+        let introduction = document.getElementById("introduction");
+        let template = document.getElementById("profile-temp");
+        let myObj = {}; 
+        let objArr = [];
+
+            arr.forEach(element => {
+                myObj.id = element.id;
+                myObj.contact_time = element.contact_time;
+                myObj.appeal = element.appeal;
+                objArr.push(myObj);
+
+                let p = document.importNode(template.content, true);
+                let pName = p.querySelector("h3");
+                pName.innerText = myObj.id;
+
+                let pIntroduction = pName.nextElementSibling;
+                pIntroduction.innerText = myObj.appeal;
+
+                container.append(p);
+            });
+        });
+}
