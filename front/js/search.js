a84ece65a5753b84603b76ba6e4921b5929e8c11
ruddus000@naver.com <ruddus000@naver.com>
2023-02-16 13:42:28 +0900

    경연 commit

--- front/js/search.js
+++ front/js/search.js
@@ -1,23 +1,22 @@
 let name = document.getElementById("name");
 let introduction = document.getElementById("introduction");
-fetch("http://localhost:8080/ohgym/find") 
-            .then((resp) => resp.text())
-            .then((text) => {
-                makeObj(text);
-                // console.log(text);
-                // const infoStr = text;
-                // const infoSplit = infoStr.split(", ");
-             
-                introduction.innerText = text;
-            });
+fetch("http://192.168.0.104:8080/ohgym-back/find")
+  .then((resp) => resp.text())
+  .then((text) => {
+    makeObj(text);
+    // console.log(text);
+    // const infoStr = text;
+    // const infoSplit = infoStr.split(", ");
 
+    introduction.innerText = text;
+  });
 
 // let teacherInfo {
-    
+
 // };
-let makeObj = function(x) {
-    const str = x;
-    const strSplit = str.split("TeacherInfo ");
-    console.log(Array.isArray(strSplit));
-    return strSplit[1];
+let makeObj = function (x) {
+  const str = x;
+  const strSplit = str.split("TeacherInfo ");
+  console.log(Array.isArray(strSplit));
+  return strSplit[1];
 };
