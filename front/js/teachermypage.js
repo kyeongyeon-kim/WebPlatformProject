cf1f8d779b1e5e21d3bbae707acb0007dd6089f4
ruddus000@naver.com <ruddus000@naver.com>
2023-02-20 17:41:08 +0900

    경연 commit

--- front/js/teachermypage.js
+++ front/js/teachermypage.js
@@ -1,13 +1,23 @@
-const inputCost = document.getElementById("input-cost");
-input.addEventListener("keyup", function (e) {
-  let value = e.target.value;
-  value = Number(value.replaceAll(",", ""));
-  if (isNaN(value)) {
-    //NaN인지 판별
-    input.value = 0;
-  } else {
-    //NaN이 아닌 경우
-    const formatValue = value.toLocaleString("ko-KR");
-    input.value = formatValue;
-  }
+// 금액 input 넘버포맷 (미구현)
+
+// 견적서 리스트 로드
+window.addEventListener("load", (e) => {
+  fetch("http://localhost:8080/ohgym/requestList")
+    .then((resp) => resp.json())
+    .then((arr) => {
+      let requestList = document.getElementById("requestList");
+      let requestCard = document.getElementById("request-card");
+      let template = document.getElementById("request-card-temp");
+
+      console.log(requestList);
+      console.log(requestCard);
+      console.log(template);
+      arr.forEach((elem) => {
+        let importTemplate = document.importNode(
+          template.contentEditable,
+          true
+        );
+        requestList.append(importTemplate);
+      });
+    });
 });
