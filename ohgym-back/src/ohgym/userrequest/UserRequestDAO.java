019e85cfbb6aaf1801fa658c6fa108d9ee0aaab0
tree075964@gmail.com <ruddus000@naver.com>
2023-02-20 17:12:49 +0900

    요청서 코드

---
+++ ohgym-back/src/ohgym/userrequest/UserRequestDAO.java
@@ -0,0 +1,83 @@
+package ohgym.userrequest;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import ohgym.dbutil.ConnectionProvider;
+
+public class UserRequestDAO {
+	public void requestAdd(String user_id, String exercise_type, String str) {
+        LocalDateTime RequestDate = LocalDateTime.now();
+        LocalDateTime DeadlineDate = RequestDate.plusHours(72);
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String RequestDateStr = RequestDate.format(formatter);
+        String DeadlineDateaStr = DeadlineDate.format(formatter);
+
+		String sql = "INSERT INTO request (user_id, exercise_type, request_date, deadline_date, message) VALUES ('"
+				+ user_id + "', '"
+				+ exercise_type + "', '" 
+				+ RequestDateStr + "', '"
+				+ DeadlineDateaStr + "', '"
+				+ str + "')";
+		
+		try (Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql)){
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void requestAnswerAddData(String[] arr) {
+		String number = null;
+		try (Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement stmt = conn.prepareStatement("SELECT no FROM request ORDER BY no DESC LIMIT 1")){
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				number = rs.getString("no");
+			}
+			stmt.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		String page = null;
+		String sql = null;
+		
+		for (int i = 0; i < 38; i++) {
+			if (i < 9){
+				page = "1";
+			} else if (i < 14) {
+				page = "2";
+			} else if (i < 22) {
+				page = "3";
+			} else if (i < 28) {
+				page = "4";
+			} else if (i < 31) {
+				page = "5";
+			} else if (i < 37) {
+				page = "6";
+			} else {
+				page = "7";
+			}
+			if (!arr[i].equals("null")) {
+				sql = "INSERT INTO request_answer (request_no, question, answer) VALUES ("+ number + ", " + page + ", '"+ arr[i] + "');";
+				requestAnswerAddData(sql);
+			}
+		}
+	}
+	
+	public void requestAnswerAddData(String sql) {
+		try (Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(sql)){
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ohgym-back/src/ohgym/userrequest/UserResponse.java
@@ -0,0 +1,40 @@
+package ohgym.userrequest;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebServlet("/userresp")
+public class UserResponse extends HttpServlet {
+	@Override
+	  protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+	    try {
+	    	UserResponseDAO userRespDao = new UserResponseDAO();
+	    	String category = request.getParameter("category");
+	    	String categoryName = userRespDao.categoryName(category);
+	    	String requestNum = userRespDao.requestNum(category);
+	    	String scoreNum = userRespDao.scoreNum(category);
+	    	String reviewNum = userRespDao.reviewNum(category);
+	    	String activeNum = userRespDao.activeNum(category);
+	    	List<String> list = new ArrayList<String>();
+	    	list.add(requestNum);
+	    	list.add(scoreNum);
+	    	list.add(reviewNum);
+	    	list.add(activeNum);
+	    	list.add(categoryName);
+			ObjectMapper mapper = new ObjectMapper();
+	    	String json = mapper.writeValueAsString(list);
+	    	
+	    	response.getWriter().write(json);
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	}
+}
