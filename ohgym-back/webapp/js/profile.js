5137f9f1565fd3f006b07e6cc68411bf283570e8
ruddus000@naver.com <ruddus000@naver.com>
2023-02-28 17:21:50 +0900

    폴더 병합

--- ohgym-back/webapp/js/profile.js
+++ ohgym-back/webapp/js/profile.js
@@ -1,57 +1,227 @@
 // 로그인 사용자 값 불러오기
-let userId = document.getElementById("userId");
-userId.innerText = "경연";
-window.addEventListener("load", (e) => {
-  userId.innerText = userId.innerText + " 근선생님!";
-  fetch("http://192.168.0.104:8080/ohgym/profile")
-    .then((resp) => resp.json())
-    .then((arr) => {
-      arr.forEach((element) => {
-        console.log(element);
-      });
-    });
-});
-function getId() {}
-
-// 수정 버튼 이벤트 처리 파트
-let modify = document.querySelectorAll(".heading div:nth-child(2)");
-let values = document.querySelectorAll(".value");
-let comps = document.querySelectorAll("#comps > .info-comp");
+var profile;
+let profileImage = document.getElementById("profile-image");
 let template = document.getElementById("input-form");
-let modifyArr = [...modify];
-let valuesArr = [...values];
-let compsArr = [...comps];
-let value = [];
-modifyArr.forEach((elem) => {
-  elem.addEventListener("click", function (e) {
-    let index = modifyArr.indexOf(elem);
-    let modifyStr = modifyArr[index].innerText;
-    let valueText = valuesArr[index].innerText;
-    if (modifyStr !== "저장") {
-      modifyArr[index].innerText = "저장";
-      modifyArr[index].style.color = "RED";
-
-      let importTemplate = document.importNode(template.content, true);
-      value[index] = valuesArr[index];
-      valuesArr[index].remove();
-      let input = importTemplate.querySelector("input");
-      input.value = valueText;
-      comps[index].append(importTemplate);
-    } else {
-      let article = document.querySelector(
-        "#comps > .info-comp:nth-child(" + (index + 1) + ") > article"
-      );
-      article.remove();
-      let className = modifyArr[index].getAttribute("class");
-
-      if (className === "modify") {
-        modifyArr[index].innerText = "수정";
-      } else {
-        modifyArr[index].innerText = "등록하기";
-      }
-
-      modifyArr[index].style.color = "#00c7ae";
-      compsArr[index].append(value[index]);
-    }
-  });
+window.addEventListener("load", () => {
+  fetch("http://localhost:8080/ohgym/profile")
+    .then((resp) => resp.json())
+    .then((profile) => {
+      this.profile = profile;
+      setInitialValue();
+    })
+    .catch();
 });
+
+function setInitialValue() {
+  userId.innerText = profile.id + " 근선생님!";
+  profileImage.setAttribute("src", profile.image);
+  stringToTime();
+  stringToAddress();
+  document.getElementById("appeal").innerText = profile.appeal;
+  document.getElementById("introduction").innerText = profile.introduction;
+  document.getElementById("centerName").innerText = profile.centerName;
+  careerToNumber(profile.career);
+  serviceInitialValue();
+}
+
+// db 저장된 대표서비스 값으로 초기버튼 checked
+function serviceInitialValue() {
+  let serviceBtns = document.querySelectorAll(".service-btn");
+  let serviceBtnsArr = [...serviceBtns];
+  for (let i = 0; i < serviceBtnsArr.length; i++) {
+    if (serviceBtnsArr[i].innerText == profile.exercise) {
+      let input = serviceBtnsArr[i].previousSibling;
+      input.checked = true;
+    }
+  }
+}
+
+// 수정 및 저장 이벤트 로직
+function returnModifyComp(saveBtn, component) {
+  saveBtn.innerText = "저장";
+  let importTemplate = document.importNode(template.content, true);
+  importTemplate.querySelector("input").value = component.innerText;
+  component.style.display = "none";
+  return importTemplate;
+}
+
+function setModifiedValue(saveBtn, component, componentParent) {
+  let div = componentParent.querySelector("div:nth-child(3)");
+  let input = div.querySelector("input");
+  if (div !== null) {
+    saveBtn.innerText = "저장완료";
+    setTimeout(() => (saveBtn.innerText = "수정"), 3000);
+    component.innerText = input.value;
+    profile.component = input.value;
+    div.remove();
+  }
+  component.style.display = "block";
+}
+
+// appeal(한줄소개) 이벤트 처리
+let appealSave = document.getElementById("appeal-save");
+appealSave.addEventListener("click", changeAppealSave);
+
+function changeAppealSave() {
+  let appeal = document.getElementById("appeal");
+  let infoComp = appeal.parentNode;
+  if (appealSave.innerText === "수정") {
+    infoComp.append(returnModifyComp(appealSave, appeal));
+  } else if (appealSave.innerText === "저장") {
+    setModifiedValue(appealSave, appeal, infoComp);
+  }
+  sendProfile();
+}
+
+// 시간 저장하는 이벤트 처리
+let timeSave = document.getElementById("time-save");
+timeSave.addEventListener("click", changeTimeSave);
+
+function changeTimeSave() {
+  timeSave.innerText = "저장완료";
+  setTimeout(() => (timeSave.innerText = "저장"), 3000);
+  profile.contactTime = timeToString();
+  sendProfile();
+}
+let infoContactTime = document.getElementById("info-contactTime");
+infoContactTime.addEventListener("change", function (e) {
+  timeSave.innerText = "저장";
+});
+// 시간 변환
+function timeToString() {
+  let startTime = document.getElementById("start-time");
+  let endTime = document.getElementById("end-time");
+  let profileTime = startTime.value + "-" + endTime.value;
+  return profileTime;
+}
+
+function stringToTime() {
+  let arr = profile.contactTime.split("-");
+  let start = arr[0];
+  let end = arr[1];
+  let startTime = document.getElementById("start-time");
+  startTime.value = start;
+  let endTime = document.getElementById("end-time");
+  endTime.value = end;
+}
+
+// 대표서비스 이벤트 처리
+let services = document.querySelectorAll(".service-btn");
+let servicesArr = [...services];
+servicesArr.forEach((elem) => {
+  elem.addEventListener("click", selectedService);
+});
+function selectedService() {
+  profile.exercise = this.innerText;
+  sendProfile();
+}
+
+// introduction 이벤트 처리
+let introductionSave = document.getElementById("introduction-save");
+introductionSave.addEventListener("click", changeIntroductionSave);
+
+function changeIntroductionSave() {
+  let introduction = document.getElementById("introduction");
+  let infoComp = introduction.parentNode;
+  if (introductionSave.innerText === "수정") {
+    infoComp.append(returnModifyComp(introductionSave, introduction));
+  } else if (introductionSave.innerText === "저장") {
+    setModifiedValue(introductionSave, introduction, infoComp);
+  }
+  sendProfile();
+}
+
+// centerName 이벤트 처리
+let centerNameSave = document.getElementById("centerName-save");
+centerNameSave.addEventListener("click", changeCenterNameSave);
+
+function changeCenterNameSave() {
+  let centerName = document.getElementById("centerName");
+  let infoComp = centerName.parentNode;
+  if (centerNameSave.innerText === "수정") {
+    infoComp.append(returnModifyComp(centerNameSave, centerName));
+  } else if (centerNameSave.innerText === "저장") {
+    setModifiedValue(centerNameSave, centerName, infoComp);
+  }
+  sendProfile();
+}
+
+// 위치 이벤트 처리
+let addressSave = document.getElementById("address-save");
+addressSave.addEventListener("click", changeAddressSave);
+
+function changeAddressSave() {
+  addressSave.innerText = "저장완료";
+  setTimeout(() => (addressSave.innerText = "저장"), 3000);
+  profile.location = addressToString();
+  sendProfile();
+}
+
+// 주소 변환
+function addressToString() {
+  let address = document.getElementById("input-address");
+  let detailedAddress = document.getElementById("input-detailed-address");
+  let profileAddress = address.value + "/" + detailedAddress.value;
+  return profileAddress;
+}
+
+function stringToAddress() {
+  let arr = profile.location.split("/");
+  let address = arr[0];
+  let detailedAddress = arr[1];
+  let inputAddress = document.getElementById("input-address");
+  inputAddress.value = address;
+  let inputDetailedAddress = document.getElementById("input-detailed-address");
+  inputDetailedAddress.value = detailedAddress;
+}
+
+// 경력 이벤트 처리
+let inputCareer = document.getElementById("input-career");
+let inputCareerMonth = document.getElementById("input-career-month");
+inputCareer.addEventListener("wheel", (event) => {
+  let wheel = event.wheelDeltaY;
+  console.log("inputCareer" + inputCareer.value);
+  if (wheel > 0) {
+    console.log("Up!");
+  } else {
+    // (wheel < 0)
+    console.log("Down!");
+  }
+});
+
+let careerSave = document.getElementById("career-save");
+careerSave.addEventListener("click", changeCareerSave);
+
+function changeCareerSave() {
+  careerSave.innerText = "저장완료";
+  setTimeout(() => (careerSave.innerText = "저장"), 3000);
+  profile.career = numberToCareer(inputCareer.value, inputCareerMonth.value);
+  sendProfile();
+}
+
+function careerToNumber(career) {
+  document.getElementById("input-career").value = career.substring(
+    0,
+    career.indexOf("년")
+  );
+  document.getElementById("input-career-month").value = career.substring(
+    career.indexOf("년") + 1,
+    career.indexOf("개")
+  );
+}
+
+function numberToCareer(yearNumber, monthNumber) {
+  console.log(yearNumber + "년" + monthNumber + "개월");
+  return yearNumber + "년" + monthNumber + "개월";
+}
+
+// 프로필 전송
+function sendProfile() {
+  fetch("http://localhost:8080/ohgym/profile", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(profile),
+  });
+}
--- ohgym-back/webapp/js/search.js
+++ ohgym-back/webapp/js/search.js
@@ -1,32 +1,114 @@
 window.addEventListener("load", onload);
-
 function onload(e) {
-  console.log("페이지 로딩 후 호출됩니다.");
-
-  fetch("http://192.168.0.104:8080/ohgym/find")
+    console.log("페이지 로딩 후 호출됩니다.");
+    fetch("http://192.168.0.104:8080/ohgym/find") 
     .then((resp) => resp.json())
     .then((arr) => {
-      let container = document.getElementById("profile-container");
-      let name = document.getElementById("name");
-      let introduction = document.getElementById("introduction");
-      let template = document.getElementById("profile-temp");
-      let myObj = {};
-      let objArr = [];
-
-      arr.forEach((element) => {
-        myObj.id = element.id;
-        myObj.contact_time = element.contact_time;
-        myObj.appeal = element.appeal;
-        objArr.push(myObj);
-
-        let p = document.importNode(template.content, true);
-        let pName = p.querySelector("h3");
-        pName.innerText = myObj.id;
-
-        let pIntroduction = pName.nextElementSibling;
-        pIntroduction.innerText = myObj.appeal;
-
-        container.append(p);
-      });
+        arr.forEach(element => {
+            makeObj(element);
+            fillProfile(element);
+        });             
     });
 }
+
+let objArr = [];
+function makeObj(element) {
+    let myObj = {}; 
+    myObj.id = element.id;
+    myObj.appeal = element.appeal;
+    myObj.contactTime = element.contactTime;
+    myObj.exercise = element.exercise;
+    myObj.introduction = element.introduction;
+    myObj.centerName = element.centerName;
+    myObj.location = element.location;
+    myObj.career = element.career;
+    myObj.image = element.image;
+    objArr.push(myObj);
+}
+
+function fillProfile(element) {
+    let container = document.getElementById("profile-container");
+    let template = document.getElementById("profile-temp");
+    let p = document.importNode(template.content, true);
+    let pId = p.querySelector("h3");
+    pId.innerText = element.id;
+    let pAppeal = p.querySelector("#appeal");
+    pAppeal.innerText = element.appeal;
+    let pIntroduction = p.querySelector("#introduction");
+    pIntroduction.innerText = element.introduction;
+    let pContactTime = p.querySelector("#contactTime");
+    pContactTime.innerText = element.contactTime; 
+    let pCenterLocation = p.querySelector("#centerLocation");
+    pCenterLocation.innerText = element.location; 
+    let pImage = p.querySelector("#image");
+    pImage.src = element.image;
+    let profile = p.querySelector(".profile");
+    profile.id = element.id + element.exercise;
+    container.append(p);
+}
+
+let type = document.getElementById("type");
+type.addEventListener("change", choiceType);
+function choiceType() {
+    let selectType = type.options[type.selectedIndex].text;
+    console.log(selectType);
+    objArr.forEach(myObj => {
+        let profile = document.getElementById(myObj.id + myObj.exercise);
+        if (selectType == myObj.exercise) {
+            profile.style.display = "block";
+        } else if (selectType == "서비스 선택") {
+            profile.style.display = "block";
+        } else {
+            profile.style.display = "none";
+        }
+    })
+}
+
+let area = document.getElementById("area");
+area.addEventListener("change", choiceArea);
+function choiceArea() {
+    let selectArea = area.options[area.selectedIndex].text;
+    console.log(selectArea);
+    objArr.forEach(myObj => {
+        let centerArea = myObj.location;
+        let trueArea = centerArea.search(selectArea)
+        let profile = document.getElementById(myObj.id + myObj.exercise);
+        console.log(trueArea);
+        if (trueArea == 0) {
+            profile.style.display = "block";
+        } else if (selectArea == "지역") {
+            profile.style.display = "block";
+        } else {
+            profile.style.display = "none";
+        }
+    })
+}
+
+let searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", searchInput);
+function searchInput() {
+    let text = getInput();
+    objArr.forEach(myObj => {
+        let match = JSON.stringify(myObj).search(text)
+        console.log(match);
+        let profile = document.getElementById(myObj.id + myObj.exercise);
+        if (match == -1) {
+            profile.style.display = "none";
+        } else {
+            profile.style.display = "block";
+        }
+    })
+}
+let input = document.getElementById("search");
+input.addEventListener("change", getInput);
+input.addEventListener("keydown", function(){
+    if (window.event.keyCode == 13) {
+        searchInput();
+    }
+});
+function getInput() {
+    input = document.getElementById("search").value;
+    console.log(input);
+    return input;
+}
+
