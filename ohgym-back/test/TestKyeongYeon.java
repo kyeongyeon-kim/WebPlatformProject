dd3610e39473a67a01b53927fe0bab0cc83b4ddb
ruddus000@naver.com <ruddus000@naver.com>
2023-03-02 17:49:58 +0900

    경연 commit

--- ohgym-back/test/TestKyeongYeon.java
+++ ohgym-back/test/TestKyeongYeon.java
@@ -1,8 +1,13 @@
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
+import ohgym.request.Request;
+import ohgym.request.RequestDAOImpl;
+import ohgym.request.RequestService;
+import ohgym.request.RequestServiceImpl;
 import ohgym.userrequest.RequestInfo;
 import ohgym.userrequest.RequestInfoDAO;
 
@@ -10,8 +15,27 @@ import ohgym.userrequest.RequestInfoDAO;
 public class TestKyeongYeon {
 		@Test
 		public void filterTest() {
+			String str = "부산 부산진구 중앙대로 749 혜도빌딩 4층 그린컴퓨터아카데미";
+			RequestService service = new RequestServiceImpl(new RequestDAOImpl());
+			
+			List<Request> reqList = service.selectRequest();
+			// 사용자 > 고수 (견적서 ALL)
 			RequestInfoDAO dao = new RequestInfoDAO();
 			List<RequestInfo> list = dao.requestInfoList("경연");
-			System.out.println(list);
+			for (RequestInfo requestInfo : list) {
+				System.out.println(requestInfo);
+			}
+			
+			for (Request request : reqList) {
+				System.out.println(request);
+			}
+			
+			for (RequestInfo requestInfo : list) {
+				if(requestInfo.getAnswer7().equals(str.substring(0, 2))) {
+					System.out.println(requestInfo.getUser_id());
+				}
+			}
+//			List<Request> requestList = service.selectRequestById();
+//			System.out.println(requestList);
 		}
 }
