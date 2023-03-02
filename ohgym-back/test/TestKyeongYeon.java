6d77b22229ca6630a9871882017cc94a395f75bc
ruddus000@naver.com <ruddus000@naver.com>
2023-03-02 23:25:46 +0900

    경연 commit

--- ohgym-back/test/TestKyeongYeon.java
+++ ohgym-back/test/TestKyeongYeon.java
@@ -18,23 +18,31 @@ public class TestKyeongYeon {
 			String str = "부산 부산진구 중앙대로 749 혜도빌딩 4층 그린컴퓨터아카데미";
 			RequestService service = new RequestServiceImpl(new RequestDAOImpl());
 			
-			List<Request> reqList = service.selectRequest();
-			// 사용자 > 고수 (견적서 ALL)
 			RequestInfoDAO dao = new RequestInfoDAO();
-			List<RequestInfo> list = dao.requestInfoList("경연");
-			for (RequestInfo requestInfo : list) {
-				System.out.println(requestInfo);
+			
+			
+			List<Request> afterFilterList = new ArrayList<>();
+			List<Request> reqList = service.selectRequest();
+			for (int i = 0; i < reqList.size(); i++) {
+				for (RequestInfo requestInfo : dao.requestInfoList(reqList.get(i).getId())) {
+					System.out.println(requestInfo);
+					if(requestInfo.getAnswer7() != null
+							&& reqList.get(i).getExerciseType().equals("퍼스널트레이닝")
+							&& requestInfo.getAnswer7().equals(str.substring(0, 2))) {
+						afterFilterList.add(reqList.get(i));
+					} 
+				}
 			}
 			
-			for (Request request : reqList) {
+			for (Request request : afterFilterList) {
 				System.out.println(request);
 			}
 			
-			for (RequestInfo requestInfo : list) {
-				if(requestInfo.getAnswer7().equals(str.substring(0, 2))) {
-					System.out.println(requestInfo.getUser_id());
-				}
-			}
+//			for (RequestInfo requestInfo : list) {
+//				if(requestInfo.getAnswer7() != null && requestInfo.getAnswer7().equals(str.substring(0, 2))) {
+//					System.out.println(requestInfo.getUser_id());
+//				}
+//			}
 //			List<Request> requestList = service.selectRequestById();
 //			System.out.println(requestList);
 		}
