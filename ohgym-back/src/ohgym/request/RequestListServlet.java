ce27476f987517b2547654154e10fe3d372c6beb
ruddus000@naver.com <ruddus000@naver.com>
2023-03-03 15:29:58 +0900

    견적서 필터링 작업 완료 경연 commit

--- ohgym-back/src/ohgym/request/RequestListServlet.java
+++ ohgym-back/src/ohgym/request/RequestListServlet.java
@@ -2,6 +2,10 @@ package ohgym.request;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -12,22 +16,52 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import ohgym.teacher.TeacherDAOImpl;
-import ohgym.teacher.TeacherService;
-import ohgym.teacher.TeacherServiceImpl;
+import ohgym.userrequest.RequestInfo;
+import ohgym.userrequest.RequestInfoDAO;
 
 @WebServlet("/requestList")
 public class RequestListServlet extends HttpServlet {
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		RequestService service = new RequestServiceImpl(new RequestDAOImpl());
-		List<Request> list = service.selectRequest();
+		RequestInfoDAO dao = new RequestInfoDAO();
+		List<Request> FilteredtList = new ArrayList<>();
+		
+		List<Request> requestList = service.selectRequest();
+		for (Request request : requestList) {
+			for (RequestInfo requestInfo : dao.requestInfoList(request.getId())) {
+				if(isValidRequest(request, requestInfo)) {
+					FilteredtList.add(request);
+				}
+			}
+		}
+
 		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(list); 
+		String json = mapper.writeValueAsString(FilteredtList);
 		System.out.println(json);
 		PrintWriter pw = resp.getWriter();
 		pw.println(json);
 		pw.flush();
+	}
+
+	private boolean isValidRequest(Request request, RequestInfo requestInfo) {
+		String str = "부산 부산진구 중앙대로 749 혜도빌딩 4층 그린컴퓨터아카데미";
+		Calendar cal = Calendar.getInstance();
+		cal.set(2023, 1, 22);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		
+		try {
+			if(requestInfo.getAnswer7() != null
+					&& dateFormat.parse(requestInfo.getRequest_date()).compareTo(cal.getTime()) <= 0
+					&& dateFormat.parse(requestInfo.getDeadline_date()).compareTo(cal.getTime()) >= 0
+					&& request.getExerciseType().equals("퍼스널트레이닝")
+					&& requestInfo.getAnswer7().equals(str.substring(0, 2))) {
+				return true;
+			}
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		} 
+		return false;
 	}
 }
--- ohgym-back/test/TestKyeongYeon.java
+++ ohgym-back/test/TestKyeongYeon.java
@@ -1,5 +1,10 @@
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
@@ -14,19 +19,24 @@ import ohgym.userrequest.RequestInfoDAO;
 
 public class TestKyeongYeon {
 		@Test
-		public void filterTest() {
+		public void filterTest() throws ParseException {
+			Calendar cal = Calendar.getInstance();
+			cal.set(2023, 1, 22);
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			String str = "부산 부산진구 중앙대로 749 혜도빌딩 4층 그린컴퓨터아카데미";
 			RequestService service = new RequestServiceImpl(new RequestDAOImpl());
 			
 			RequestInfoDAO dao = new RequestInfoDAO();
 			
-			
 			List<Request> afterFilterList = new ArrayList<>();
 			List<Request> reqList = service.selectRequest();
+			
 			for (int i = 0; i < reqList.size(); i++) {
 				for (RequestInfo requestInfo : dao.requestInfoList(reqList.get(i).getId())) {
-					System.out.println(requestInfo);
+					
 					if(requestInfo.getAnswer7() != null
+							&& dateFormat.parse(requestInfo.getRequest_date()).compareTo(cal.getTime()) <= 0
+							&& dateFormat.parse(requestInfo.getDeadline_date()).compareTo(cal.getTime()) >= 0
 							&& reqList.get(i).getExerciseType().equals("퍼스널트레이닝")
 							&& requestInfo.getAnswer7().equals(str.substring(0, 2))) {
 						afterFilterList.add(reqList.get(i));
